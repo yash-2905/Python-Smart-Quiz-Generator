f9faa88706bf71384bfb3b194bf16f1ce87c71d7
Shivansh Mishra <shivanshmishra27@gmail.com>
2026-04-04 11:36:34 +0530

    deployment ready

--- Quizapp/settings.py
+++ Quizapp/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9=sk&3x%ajx$m73jkd*#yy#06k4%4x)c45udf3^=hej@(dm9iv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,7 +120,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS =[
-    BASE_DIR/"static"
+    os.path.join(BASE_DIR,"static")
 ]
 
-STATIC_ROOT = BASE_DIR/'staticfiles'
+STATIC_ROOT =os.path.join( BASE_DIR,'staticfiles')
